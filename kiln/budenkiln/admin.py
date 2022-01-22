256aff273cbcd02af71f811322aa4fdf1868abcf
Benedikt <benedikt.bartl@diebartls.de>
2022-01-22 19:47:32 +0100

    Add models to admin page for utility

--- kiln/budenkiln/admin.py
+++ kiln/budenkiln/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Kiln)
+admin.site.register(TemperatureCurve)
+admin.site.register(TemperaturePoint)
