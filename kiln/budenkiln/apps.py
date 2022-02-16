1391839487a3785b1a28ee3fe8d8c065710e25d0
Benedikt <benedikt.bartl@diebartls.de>
2022-02-16 20:57:37 +0100

    Add hardware_controller communication stub

--- kiln/budenkiln/apps.py
+++ kiln/budenkiln/apps.py
@@ -1,4 +1,5 @@
 from asyncio.subprocess import PIPE
+from asyncio.windows_events import NULL
 from socket import timeout
 from django.apps import AppConfig
 import subprocess
@@ -8,9 +9,15 @@ import sys
 class BudenkilnConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'budenkiln'
+    # TODO how to access in view
+    hardware_controller = NULL
 
     def ready(self):
-        p = subprocess.Popen([sys.executable, 'hardware_controller.py'], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
-        # TODO communication
-        #p.stdin.write('Test')
-        print(p.wait())
+        if (self.hardware_controller is NULL):
+            self.hardware_controller = subprocess.Popen([sys.executable, 'hardware_controller.py'], stdin=PIPE, stdout=PIPE, stderr=PIPE, text=True)
+            # TODO communication
+            self.hardware_controller.stdin.write('Test\n')
+            self.hardware_controller.stdin.flush()
+            result = self.hardware_controller.stdout.readline()
+            print(result)
+            #print(p.wait())
--- kiln/hardware_controller.py
+++ kiln/hardware_controller.py
@@ -1,3 +1,21 @@
-from asyncore import read
+from threading import Thread
+from threading import Semaphore
+import sys
 
-exit(0)
+val = ""
+sema = Semaphore(value=1)
+
+def input_handler():
+    global val
+    while True:
+        data = sys.stdin.readline()
+        sys.stdout.write("Reply: " + str(data))
+        sys.stdout.flush()
+        sema.acquire()
+        val = 10
+        sema.release()
+
+thread = Thread(target=input_handler)
+thread.start()
+thread.join()
+exit(val)
