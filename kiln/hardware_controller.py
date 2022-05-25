6a48e05097b7d6d99cf8243d0c7a3b04f56f6423
Benedikt <benedikt.bartl@diebartls.de>
2022-05-25 23:06:11 +0200

    Add proper heater shutdown on exception

--- kiln/hardware_controller.py
+++ kiln/hardware_controller.py
@@ -20,6 +20,14 @@ class Controller(Thread):
         self._input_queue = Queue()
         self._temperature_history = dict()
         super(Controller, self).__init__()
+        # Setup Hardware IO
+        spi = board.SPI()
+        cs = DigitalInOut(board.CE0)
+        self.max31855 = adafruit_max31855.MAX31855(spi, cs)
+        self.relais = DigitalInOut(board.D15)
+        self.relais.direction = Direction.OUTPUT
+
+        self.relais.value = False
 
     def set_temp_curve(self, curve):
         print("Set Curve: ", curve)
@@ -28,99 +36,99 @@ class Controller(Thread):
     def get_temp_history(self):
         return self._temperature_history
 
-    def run(self):
-        # Setup Hardware IO
-        spi = board.SPI()
-        cs = DigitalInOut(board.CE0)
-        max31855 = adafruit_max31855.MAX31855(spi, cs)
-        relais = DigitalInOut(board.D15)
-        relais.direction = Direction.OUTPUT
+    def relais_off(self):
+        self.relais.value = False
 
+    def run(self):
         # Start with relais off
-        relais.value = False
+        self.relais.value = False
 
         curve = {}
         start_time = time()
 
-        # remove once proper thermocouple arrives
+        # required for EMI resistance
         thermocouple_error_count=0
 
-        while True:
-            # Check for new input curve
-            try:
-                new_curve = self._input_queue.get_nowait()
-                print("GOT", new_curve)
-                curve = new_curve
-                start_time = time()
-                self._temperature_history = dict()
-            except Empty:
-                pass
+        try:
+            while True:
+                # Check for new input curve
+                try:
+                    new_curve = self._input_queue.get_nowait()
+                    print("GOT", new_curve)
+                    curve = new_curve
+                    start_time = time()
+                    self._temperature_history = dict()
+                except Empty:
+                    pass
 
-            if len(curve) < 2:
-                print("No curve set!")
-                sleep(1)
-                continue
+                if len(curve) < 2:
+                    print("No curve set!")
+                    sleep(1)
+                    continue
 
-            # Fetch temperature from probe
-            try:
-                measured_temperature = max31855.temperature
-                print("Measured Temperature = {}".format(measured_temperature))
-                # remove once new thermocouple arrives
-                thermocouple_error_count=0
-            except RuntimeError:
-                # For some reason casing of thermocouple is connected to one terminal so sometimes when it first touches
-                # the kiln metal casing it gets confused - error in case there is an actual connection to ground or smth
-                # remove once new thermocouple arrives
-                print("Thermocouple error")
-                thermocouple_error_count+=1
-                if (thermocouple_error_count > 15):
-                    relais.value = False
-                    raise RuntimeError("Thermocouple short to ground!")
-                sleep(0.1)
-                continue
+                # Fetch temperature from probe
+                try:
+                    measured_temperature = self.max31855.temperature
+                    print("Measured Temperature = {}".format(measured_temperature))
+                    thermocouple_error_count=0
+                except RuntimeError:
+                    # Signal Noise due to EMI will occasionally confuse the MAX31855
+                    # Assume actual error if error persists for long duration
+                    print("Thermocouple error")
+                    thermocouple_error_count+=1
+                    if (thermocouple_error_count > 5):
+                        self.relais.value = False
+                        raise RuntimeError("Thermocouple short to ground!")
+                    sleep(1)
+                    continue
 
-            current_second = time() - start_time
-            # Log temperature
-            self._temperature_history[current_second] = measured_temperature
+                current_second = time() - start_time
+                # Log temperature
+                self._temperature_history[current_second] = measured_temperature
 
-            # Find start and end of current interval
-            timestamps = sorted(curve.keys())
-            first_point = second_point = list(timestamps)[0]
-            for timestamp in timestamps:
-                if current_second >= timestamp:
-                    first_point = timestamp
-                    # ensures second_point is never before first in case last point in list is first_point
-                    second_point = timestamp
+                # Find start and end of current interval
+                timestamps = sorted(curve.keys())
+                first_point = second_point = list(timestamps)[0]
+                for timestamp in timestamps:
+                    if current_second >= timestamp:
+                        first_point = timestamp
+                        # ensures second_point is never before first in case last point in list is reached
+                        second_point = timestamp
+                    else:
+                        second_point = timestamp
+                        break
+
+                # Find previous and next target temperature
+                first_temp = curve[first_point]
+                second_temp = curve[second_point]
+
+                # Interpolate target temperature based on start and end temperature with current time
+                points_temp_difference = second_temp - first_temp
+                points_time_difference = second_point - first_point
+                time_since_first_point = current_second - first_point
+
+                if (points_time_difference > 0):
+                    target_temp = first_temp + (points_temp_difference * (time_since_first_point / points_time_difference) )
                 else:
-                    second_point = timestamp
-                    break
+                    # Curve is finished. Maintain last temperature setting
+                    target_temp = second_temp
 
-            # Find previous and next target temperature
-            first_temp = curve[first_point]
-            second_temp = curve[second_point]
+                #print(f"First Temp at Time {first_point} = {first_temp}")
+                #print(f"Second Temp at Time {second_point} = {second_temp}")
+                #print(f"Target Temp at Time {current_second} = {target_temp}")
+                absolute_accepted_error = target_temp * 0.01
 
-            # Interpolate target temperature based on start and end temperature with current time
-            points_temp_difference = second_temp - first_temp
-            points_time_difference = second_point - first_point
-            time_since_first_point = current_second - first_point
+                if measured_temperature < (target_temp - absolute_accepted_error):
+                    self.relais.value = True
+                elif measured_temperature > (target_temp + absolute_accepted_error):
+                    self.relais.value = False
 
-            if (points_time_difference > 0):
-                target_temp = first_temp + (points_temp_difference * (time_since_first_point / points_time_difference) )
-            else:
-                # Curve is finished. Maintain last temperature setting
-                target_temp = second_temp
-
-            #print(f"First Temp at Time {first_point} = {first_temp}")
-            #print(f"Second Temp at Time {second_point} = {second_temp}")
-            print(f"Target Temp at Time {current_second} = {target_temp}")
-            absolute_accepted_error = target_temp * 0.01
-
-            if measured_temperature < (target_temp - absolute_accepted_error):
-                relais.value = True
-            elif measured_temperature > (target_temp + absolute_accepted_error):
-                relais.value = False
-
-            sleep(1)
+                sleep(1)
+        except:
+            # Ensure deactivation of relais on error
+            print("control loop terminating - shutting down heater power!")
+            self.relais_off()
+            raise
 
 class KilnService(dbus.service.Object):
     def __init__(self, conn, object_path):
@@ -146,7 +154,12 @@ class KilnService(dbus.service.Object):
 
         print("Start DBus Interface")
         dbus_loop = GLib.MainLoop()
-        dbus_loop.run()
+        try:
+            dbus_loop.run()
+        except:
+            print("Controller terminating - shutting down heater power!")
+            self._controller.relais_off()
+            raise
         
 def start():
     global _kiln_service
